23a77ddd7a1081482d56f0a9f5d9408b4c4abaf3
cr0w1 <clebsonjose30@gmail.com>
2022-03-27 23:00:47 -0300

    login and registration pages

---
+++ lib/screens/login/loginScreen.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:uber_clone/screens/register/registerScreen.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const String idScreen = "login";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 35.0,
+              ),
+              Image(
+                image: AssetImage("assets/images/logo.png"),
+                width: 390.0,
+                height: 250.0,
+                alignment: Alignment.center,
+              ),
+              SizedBox(
+                height: 1.0,
+              ),
+              Text(
+                "Login as a Uber",
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontFamily: "Brand Bold",
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    SizedBox(height: 1.0),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: TextStyle(
+                          fontSize: 14.0,
+                        ),
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                      style: TextStyle(fontSize: 14.0),
+                    ),
+                    SizedBox(height: 1.0),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        labelStyle: TextStyle(
+                          fontSize: 14.0,
+                        ),
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                      style: TextStyle(fontSize: 14.0),
+                    ),
+                    SizedBox(height: 10.0),
+                    RaisedButton(
+                      color: Colors.yellow,
+                      textColor: Colors.white,
+                      child: Container(
+                        height: 50.0,
+                        child: Center(
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontFamily: "Brand Bold",
+                            ),
+                          ),
+                        ),
+                      ),
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(24.0),
+                      ),
+                      onPressed: () {},
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.pushNamedAndRemoveUntil(
+                            context, RegisterScreen.idScreen, (route) => false);
+                      },
+                      child: Text(
+                        "Do not have an Account? Register Here",
+                        style: TextStyle(fontFamily: "Brand Bold"),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
